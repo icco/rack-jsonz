d183d66611c777b489eefdbe31babe0e345afdf0
Nat Welch <nat@natwelch.com>
2014-04-20 10:13:58 -0700

    simplest middleware

--- lib/rack/jsonz.rb
+++ lib/rack/jsonz.rb
@@ -2,6 +2,20 @@ require "rack/jsonz/version"
 
 module Rack
   module Jsonz
-    # Your code goes here...
+    def initialize(app)
+      @app = app
+    end
+
+    def call(env)
+      if env["PATH_INFO"].eql? "/jsonz"
+        [
+          200,
+          { "Content-Type" => "text/plain" },
+          ["OK"]
+        ]
+      else
+        @app.call(env)
+      end
+    end
   end
 end
