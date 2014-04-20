3b6c6f3ec3204d8e1702eb8f6c32a3eef7a47d49
Nat Welch <nat@natwelch.com>
2014-04-20 13:15:04 -0700

    actually pass block

--- lib/rack/jsonz/metric.rb
+++ lib/rack/jsonz/metric.rb
@@ -1,8 +1,8 @@
 module Rack
   class Jsonz
     class Metric
-      def each_metric 
-        self.metrics.each_pair
+      def each_metric(&block)
+        self.metrics.each_pair(&block)
       end
     end
   end
